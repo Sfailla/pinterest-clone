bbffca19c079300e85420d1a18541bd9ab91f4a4
Stevo99 <Sfailla1983@gmail.com>
2019-10-21 20:00:32 -0400

    changed input for normalize.css so that styles take effect

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'normalize.css/normalize.css';
+import 'normalize.css';
 import './styles/style.scss';
 
 import Header from './components/Header';
