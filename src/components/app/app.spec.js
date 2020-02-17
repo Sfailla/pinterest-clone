5fa234fc433a16715ba9a35baa2cadb88f5b53c7
Stevo99 <Sfailla1983@gmail.com>
2020-02-17 13:51:53 -0500

    changed app to App

--- src/components/app/app.spec.js
+++ src/components/app/app.spec.js
@@ -1,6 +1,6 @@
 import React from './node_modules/react';
 import { render } from './node_modules/@testing-library/react';
-import App from './app';
+import App from './App';
 
 describe('App', () => {
 	it('renders App without error', () => {
