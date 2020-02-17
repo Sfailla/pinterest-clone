813e2b2895f03489f62bb23dcc1d0d92861d945e
Stevo99 <Sfailla1983@gmail.com>
2020-02-17 12:09:47 -0500

    cleaned up old files

--- src/app.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './app';
-
-describe('App', () => {
-	it('renders App without error', () => {
-		render(<App />);
-	});
-});
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'normalize.css';
 import './styles/style.scss';
-import App from './app';
+import App from './components/app/app';
 
 const app = <App />;
 
