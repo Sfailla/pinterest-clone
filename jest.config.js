4ac6c43220018ed34d6afb3ddefb7916cb1bf13c
Stevo99 <Sfailla1983@gmail.com>
2020-02-17 10:32:44 -0500

    fixing issues with jest test

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,4 @@
 module.exports = {
 	setupFilesAfterEnv: [ '<rootDir>/testSetup.js' ],
-	testMatch: [
-		'**/__tests__/**/*.js?(x)',
-		'**/?(*.)+(spec|test).js?(x)'
-	],
-	moduleDirectories: [ 'node_modules' ],
-	testURL: 'http://localhost',
-	moduleFileExtensions: [ 'js', 'json', 'jsx' ],
-	transformIgnorePatterns: [ '<rootDir>/node_modules/' ]
+	moduleFileExtensions: [ 'js', 'json', 'jsx' ]
 };
