26f78db853a442c7bed198fd2da60b443bebdf1d
Stevo99 <Sfailla1983@gmail.com>
2020-04-03 15:45:12 -0400

    removed unnecessary markup

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import Header from '../Header.js';
-
-const styles = {
-	content: {
-		marginTop: '5rem',
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center'
-	}
-};
+import Header from '../header/Header.js';
 
 const App = () => {
 	return (
 		<div>
 			<Header />
-			<div style={styles.content}>
-				<code>Remove / Edit Header.jsx file to get started</code>
-			</div>
 		</div>
 	);
 };
