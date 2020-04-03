0e632add42b16edf1e50d6cae975805480ed9de0
Stevo99 <Sfailla1983@gmail.com>
2020-04-03 16:08:58 -0400

    added react router to app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Header from '../header/Header.js';
+import HomePage from '../../pages/homepage/HomePage.js';
 
 const App = () => {
+	const user = null;
+
 	return (
-		<div>
-			<Header />
-		</div>
+		<BrowserRouter>
+			<div className="app-container">
+				{user && <Header />}
+				<div className="route-container">
+					<Switch>
+						<Route path="/" component={HomePage} />
+					</Switch>
+				</div>
+			</div>
+		</BrowserRouter>
 	);
 };
 
