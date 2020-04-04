059012ce4b82b16bffc01e87cb45cf9461c37038
Stevo99 <Sfailla1983@gmail.com>
2020-04-04 18:10:46 -0400

    added styles and images for dynamic masonry grid

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -10,54 +10,46 @@ import image7 from '../../images/grid-img-7.png';
 import image8 from '../../images/grid-img-8.png';
 import image9 from '../../images/grid-img-9.png';
 import image10 from '../../images/grid-img-10.png';
+import Box from '../../components/grid box/Box';
 
-const HomePage = () => {
+function HomePage() {
 	return (
 		<div className="homepage">
-			<div className="homepage__grid-container">
-				<div
-					style={{ background: `url(${image1})` }}
-					className="box box-1"
-				/>
-				<div
-					style={{ background: `url(${image2})` }}
-					className="box box-2"
-				/>
-				<div
-					style={{ background: `url(${image3})` }}
-					className="box box-3"
-				/>
-				<div
-					style={{ background: `url(${image4})` }}
-					className="box box-4"
-				/>
-				<div
-					style={{ background: `url(${image5})` }}
-					className="box box-5"
-				/>
-				<div
-					style={{ background: `url(${image6})` }}
-					className="box box-6"
-				/>
-				<div
-					style={{ background: `url(${image7})` }}
-					className="box box-7"
-				/>
-				<div
-					style={{ background: `url(${image8})` }}
-					className="box box-8"
-				/>
-				<div
-					style={{ background: `url(${image9})` }}
-					className="box box-9"
-				/>
-				<div
-					style={{ background: `url(${image10})` }}
-					className="box box-10"
-				/>
+			<div className="grid-container">
+				<Box image={image1} className="box box-large" />
+				<Box image={image2} className="box box-large" />
+				<Box image={image3} className="box box-medium" />
+				<Box image={image4} className="box box-medium" />
+				<Box image={image5} className="box box-medium" />
+				<Box image={image6} className="box box-small" />
+				<Box image={image7} className="box box-large" />
+				<Box image={image8} className="box box-small" />
+				<Box image={image9} className="box box-large" />
+				<Box image={image10} className="box box-large" />
+				<Box image={image1} className="box box-large" />
+				<Box image={image2} className="box box-large" />
+				<Box image={image3} className="box box-medium" />
+				<Box image={image4} className="box box-medium" />
+				<Box image={image5} className="box box-medium" />
+				<Box image={image6} className="box box-small" />
+				<Box image={image7} className="box box-large" />
+				<Box image={image8} className="box box-small" />
+				<Box image={image9} className="box box-large" />
+				<Box image={image10} className="box box-large" />
+				<Box image={image1} className="box box-large" />
+				<Box image={image2} className="box box-large" />
+				<Box image={image3} className="box box-medium" />
+				<Box image={image4} className="box box-medium" />
+				<Box image={image5} className="box box-medium" />
+				<Box image={image6} className="box box-small" />
+				<Box image={image7} className="box box-large" />
+				<Box image={image8} className="box box-small" />
+				<Box image={image8} className="box box-small" />
+				<Box image={image9} className="box box-large" />
+				<Box image={image10} className="box box-large" />
 			</div>
 		</div>
 	);
-};
+}
 
 export default HomePage;
