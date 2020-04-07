496f594abaa17f117c83bca0f619c1e0aa272d94
Stevo99 <Sfailla1983@gmail.com>
2020-04-07 10:23:18 -0400

    added component for Form Group and Input

---
+++ src/components/form-group/FormGroup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FormGroup = ({ children }) => {
+	return <form>{children}</form>;
+};
+
+export default FormGroup;
---
+++ src/components/input/Input.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Input = ({ ...otherProps }) => {
+	return <input {...otherProps} />;
+};
+
+export default Input;
