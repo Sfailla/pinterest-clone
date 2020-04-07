be28116f32a4bb2d270ecd2e74013b03404031ec
Stevo99 <Sfailla1983@gmail.com>
2020-04-07 10:21:03 -0400

    added component and styles for Homepage card

--- src/components/user-auth/SigninAndSignup.js
+++ src/components/user-auth/SigninAndSignup.js
@@ -1,43 +1,57 @@
 import React from 'react';
 import logo from '../../svg/pinterest-logo.svg';
 
+import FormGroup from '../form-group/FormGroup';
+import Input from '../input/Input';
+
 export const SigninAndSignup = () => {
 	return (
-		<div className="card">
-			<div className="card-body">
-				<div className="card-body-upper">
+		<div className="card signup__card">
+			<div className="card-body signup__card-body">
+				<div className="card-body-upper signup__card-body-upper">
 					<img
-						className="pinterest-logo"
+						className="pinterest-logo signup__logo"
 						src={logo}
 						alt="pinterest logo"
 					/>
-					<div className="text-wrapper">
-						<h1>Welcome to pinterest</h1>
+					<div className="signup__text-wrapper">
+						<h3>Welcome to pinterest</h3>
 						<p>Find new ideas to try</p>
 					</div>
 				</div>
-				<div className="input-wrapper">
-					<input type="text" placeholder="Email" name="email" />
-					<input
+				<FormGroup className="form-wrapper">
+					<Input
+						className="card-input"
+						type="text"
+						placeholder="Email"
+						name="email"
+					/>
+					<Input
 						type="password"
 						placeholder="Password"
+						className="card-input"
 						name="password"
 					/>
-					<input type="number" placeholder="Age" name="age" />
-				</div>
-				<div className="button-wrapper">
-					<div className="ind-btn">
+					<Input
+						type="number"
+						className="card-input"
+						placeholder="Age"
+						name="age"
+					/>
+				</FormGroup>
+				<div className="button-group">
+					<div className="button-wrapper">
 						<button className="rounded-button login-button">
 							Continue
 						</button>
 					</div>
 					<span>OR</span>
-					<div className="ind-btn">
+					<div className="button-wrapper">
 						<button className="rounded-button signup-button">
 							Continue with Facebook
 						</button>
 					</div>
-					<div className="ind-btn">
+					<div className="button-wrapper">
 						<button className="rounded-button signup-button">
 							Continue with Google
 						</button>
