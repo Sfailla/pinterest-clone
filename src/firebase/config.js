6fa8bab2f4bb8beb3ebfc13dd3f1dc127d327975
Stevo99 <Sfailla1983@gmail.com>
2020-04-08 22:41:24 -0400

    added firebase to project

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyBu39YiHOsU0qYIHlCK2h2ZAcYWFQj3w1o',
+	authDomain: 'pinterest-clone-cd38e.firebaseapp.com',
+	databaseURL: 'https://pinterest-clone-cd38e.firebaseio.com',
+	projectId: 'pinterest-clone-cd38e',
+	storageBucket: 'pinterest-clone-cd38e.appspot.com',
+	messagingSenderId: '886614119714',
+	appId: '1:886614119714:web:6ed108181eac914c86017b',
+	measurementId: 'G-P4KL4PC7EH'
+};
+
+export default firebaseConfig;
