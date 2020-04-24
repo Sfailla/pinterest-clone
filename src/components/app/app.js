629678e2371cb5ad10a677755392cbbeb0c96230
Stevo99 <Sfailla1983@gmail.com>
2020-04-24 16:40:23 -0400

    added privateRoute function and added userAuth

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+	BrowserRouter,
+	Switch,
+	Route,
+	Redirect
+} from 'react-router-dom';
+import userAuth from '../user-auth/userAuth';
 
 import Header from '../header/Header.js';
 import HomePage from '../../pages/homepage/HomePage.js';
+import Dashboard from '../../pages/dashboard/Dashboard';
 
-const App = () => {
-	let user = null;
+const App = props => {
+	const user = userAuth();
+
+	const PrivateRoute = ({ component: Component, ...rest }) => {
+		return (
+			<Route
+				{...rest}
+				render={props =>
+					!!user ? (
+						<Component {...props} />
+					) : (
+						<Redirect
+							to={{
+								pathname: '/',
+								state: { from: props.location }
+							}}
+						/>
+					)}
+			/>
+		);
+	};
 
 	return (
 		<BrowserRouter>
@@ -13,7 +39,12 @@ const App = () => {
 				{user && <Header />}
 				<div className="route-container">
 					<Switch>
-						<Route path="/" component={HomePage} />
+						<Route exact path="/" component={HomePage} />
+						<PrivateRoute
+							exact
+							path="/dashboard"
+							component={Dashboard}
+						/>
 					</Switch>
 				</div>
 			</div>
