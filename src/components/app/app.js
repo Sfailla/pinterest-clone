5f904acfb946184733fe75f9a90564b70e69359c
Stevo99 <Sfailla1983@gmail.com>
2020-04-25 10:33:03 -0400

    moved PrivateRoute to its own file and imported it to app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,37 +1,16 @@
 import React from 'react';
-import {
-	BrowserRouter,
-	Switch,
-	Route,
-	Redirect
-} from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import userAuth from '../user-auth/userAuth';
+import PrivateRoute from '../../router/PrivateRoute';
+import PublicRoute from '../../router/PublicRoute';
 
 import Header from '../header/Header.js';
 import HomePage from '../../pages/homepage/HomePage.js';
 import Dashboard from '../../pages/dashboard/Dashboard';
 
-const App = props => {
+function App() {
 	const user = userAuth();
-
-	const PrivateRoute = ({ component: Component, ...rest }) => {
-		return (
-			<Route
-				{...rest}
-				render={props =>
-					!!user ? (
-						<Component {...props} />
-					) : (
-						<Redirect
-							to={{
-								pathname: '/',
-								state: { from: props.location }
-							}}
-						/>
-					)}
-			/>
-		);
-	};
+	console.log(!!user);
 
 	return (
 		<BrowserRouter>
@@ -39,17 +18,24 @@ const App = props => {
 				{user && <Header />}
 				<div className="route-container">
 					<Switch>
-						<Route exact path="/" component={HomePage} />
-						<PrivateRoute
+						<PublicRoute
+							user={user}
 							exact
-							path="/dashboard"
-							component={Dashboard}
+							path="/"
+							component={HomePage}
 						/>
+						{user && (
+							<PrivateRoute
+								user={user}
+								path="/dashboard"
+								component={Dashboard}
+							/>
+						)}
 					</Switch>
 				</div>
 			</div>
 		</BrowserRouter>
 	);
-};
+}
 
 export default App;
