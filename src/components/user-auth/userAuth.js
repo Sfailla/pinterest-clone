b3cc2eabaab7083544d9d91c07548298ebe98671
Stevo99 <Sfailla1983@gmail.com>
2020-04-25 10:34:08 -0400

    added call to onAuthStateChanged to get status of user. if we have a user the info is sent to all components

--- src/components/user-auth/userAuth.js
+++ src/components/user-auth/userAuth.js
@@ -4,22 +4,19 @@ import firebase from '../../firebase/firebase';
 function userAuth() {
 	const [ user, setUser ] = useState(null);
 
-	useEffect(
-		() => {
-			const unsubscribe = firebase.auth.onAuthStateChanged(user => {
-				if (user) {
-					// user is signed in
-					setUser(user);
-				} else {
-					// user is not signed in
-					setUser(null);
-				}
-			});
+	useEffect(() => {
+		const unsubscribe = firebase.auth.onAuthStateChanged(user => {
+			if (user) {
+				// user is signed in
+				setUser(user);
+			} else {
+				// user is not signed in
+				setUser(null);
+			}
+		});
 
-			return () => unsubscribe();
-		},
-		[ user ]
-	);
+		return () => unsubscribe();
+	}, []);
 	return user;
 }
 
