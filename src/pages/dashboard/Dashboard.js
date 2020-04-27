cc246e031237b415002bd6113e0449a134a850a1
Stevo99 <Sfailla1983@gmail.com>
2020-04-27 10:16:40 -0400

    added useEffect to initialize dashboard component with image data

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Dashboard = () => {
-	return (
-		<div>
-			<h1>This is dash</h1>
-		</div>
-	);
-};
+import Home from '../../pages/home/Home';
+import Boards from '../../pages/boards/Boards';
+
+function Dashboard({ page }) {
+	const [ data, setData ] = React.useState(null);
+
+	const baseUrl = 'https://api.unsplash.com/search/photos?';
+	const client_id = 'qoz2rrh6ChkvTtjYQapHD8P3cXZNi2ZDpG_CD7WBoOU';
+
+	const getInitialData = async () => {
+		const result = await fetch(
+			`${baseUrl}&query=random&page=1&client_id=${client_id}`
+		);
+		const data = await result.json();
+		return setData(data.results);
+	};
+
+	const renderComponent = () => {
+		return page === 'home' ? <Home data={data} /> : <Boards />;
+	};
+
+	React.useEffect(() => {
+		getInitialData();
+	}, []);
+
+	return <div className="dashboard">{renderComponent()}</div>;
+}
 export default Dashboard;
