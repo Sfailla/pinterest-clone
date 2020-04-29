606c59366e23ba6e6d3e754b6666111d00088a22
Stevo99 <Sfailla1983@gmail.com>
2020-04-29 12:19:49 -0400

    added padding and home feed

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import GridContainer from '../../components/grid/GridContainer';
+import GridBox from '../../components/grid/GridBox';
 
-function Home() {
+function Home({ data }) {
 	const [ collection, setCollection ] = React.useState(null);
+	const [ loading, setLoading ] = React.useState(false);
 
 	return (
 		<div className="home">
-			<h1>Home Feed</h1>
+			<div className="text-wrapper">
+				<h1 className="home__title">explore</h1>
+				<p />
+			</div>
+
+			<GridContainer className="home__grid">
+				{data &&
+					data.map((res, i) => {
+						return (
+							<GridBox
+								className="box"
+								key={res.urls.regular}
+								addStyle={{
+									width: '236px',
+									background: `url(${res.urls
+										.regular}) no-repeat center top / cover`,
+									gridRowEnd: i % 2 === 0 ? 'span 3' : 'span 4'
+								}}
+							/>
+						);
+					})}
+			</GridContainer>
 		</div>
 	);
 }
