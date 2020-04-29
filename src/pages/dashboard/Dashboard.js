4a2afac8f88bddf0ea4a331d27b00e3044b6bf7b
Stevo99 <Sfailla1983@gmail.com>
2020-04-29 11:28:23 -0400

    added switch statement to renderComponent

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,25 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Home from '../../pages/home/Home';
 import Boards from '../../pages/boards/Boards';
 
 function Dashboard({ page }) {
-	const [ data, setData ] = React.useState(null);
+	const [ appData, setAppData ] = React.useState(null);
 
 	const baseUrl = 'https://api.unsplash.com/search/photos?';
 	const client_id = 'qoz2rrh6ChkvTtjYQapHD8P3cXZNi2ZDpG_CD7WBoOU';
 
 	const getInitialData = async () => {
-		const result = await fetch(
-			`${baseUrl}&query=random&page=1&client_id=${client_id}`
-		);
-		const data = await result.json();
-		return setData(data.results);
+		if (!appData) {
+			const query = 'guns';
+			const urlParams = `&query=${query}&page=1&per_page=20&client_id=${client_id}`;
+			const result = await fetch(`${baseUrl}${urlParams}`);
+			const data = await result.json();
+
+			return setAppData(data.results);
+		}
+		return appData;
 	};
 
 	const renderComponent = () => {
-		return page === 'home' ? <Home data={data} /> : <Boards />;
+		switch (page) {
+			case 'home':
+				return <Home data={appData} />;
+			case 'boards':
+				return <Boards />;
+			default:
+				return <Home />;
+		}
 	};
+
+	console.log(appData);
 
 	React.useEffect(() => {
 		getInitialData();
