c26831be3bc95679c562f03f0efe3a2077a1765f
Stevo99 <Sfailla1983@gmail.com>
2020-04-30 10:09:46 -0400

    added userAuth to app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,20 +6,17 @@ import PublicRoute from '../../router/PublicRoute';
 import Header from '../header/Header.js';
 import HomePage from '../../pages/homepage/HomePage.js';
 import Dashboard from '../../pages/dashboard/Dashboard';
+import userAuth from '../user-auth/userAuth';
 
-function App({ user }) {
-	const [ activeLink, setActiveLink ] = React.useState('home');
+function App() {
 	const [ page, setPage ] = React.useState('home');
+
+	const user = userAuth();
+
 	return (
 		<BrowserRouter>
 			<div className="app-container">
-				{user && (
-					<Header
-						setPage={setPage}
-						activeLink={activeLink}
-						setActiveLink={setActiveLink}
-					/>
-				)}
+				{user && <Header page={page} setPage={setPage} />}
 				<div className="route-container">
 					<Switch>
 						<PublicRoute
@@ -35,6 +32,7 @@ function App({ user }) {
 								path="/dashboard"
 								page={page}
 								user={user}
+								setPage={setPage}
 								component={Dashboard}
 							/>
 						)}
