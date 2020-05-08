f7466c01af3c659895f6da1091453df6c985f7a1
Stevo99 <Sfailla1983@gmail.com>
2020-05-08 15:52:40 -0400

    added buttons and styling for adding boards.  also added functionality for snackbar

--- src/pages/view/ViewPin.js
+++ src/pages/view/ViewPin.js
@@ -1,26 +1,62 @@
 import React from 'react';
 import './view.css';
 
-export default function ViewPin({ viewData }) {
+import Button from '../../components/buttons/Button';
+import ButtonGroup from '../../components/buttons/ButtonGroup';
+
+export default function ViewPin({
+	viewData,
+	addPin,
+	handleClick,
+	SlideTransition
+}) {
 	const { author, description, img, authorLink } = viewData;
 	const background = { backgroundImage: `url(${img})` };
 	return (
 		<div className="details wrapper">
 			<div className="details__card">
-				<div className="left">
+				<div className="details__left">
 					<div style={background} className="details__img" />
 				</div>
 				<div className="details__right">
-					<h1>{author.toUpperCase()}</h1>
-					<p className="details__description">{description}</p>
-					<a
-						href={authorLink}
-						target="_blank"
-						rel="noopener nofollow"
-						className="details__author-link"
-					>
-						link to author
-					</a>
+					<div className="details__text-wrapper">
+						<h1>{author.toUpperCase()}</h1>
+						<p className="details__description">
+							{description ? (
+								description
+							) : (
+								<span>-no description-</span>
+							)}
+						</p>
+					</div>
+					<div className="button-wrapper">
+						<a
+							href={authorLink}
+							target="_blank"
+							rel="noopener nofollow"
+							className="details__author-link"
+						>
+							more from author
+						</a>
+					</div>
+					<ButtonGroup className="details__button-group">
+						<div className="button-wrapper ">
+							<Button
+								className="details__button red-button"
+								name="ADD TO BOARDS"
+								onClick={() => {
+									addPin();
+									handleClick(SlideTransition);
+								}}
+							/>
+						</div>
+						<div className="button-wrapper">
+							<Button
+								className="details__button grey-button"
+								name="DOWNLOAD IMAGE"
+							/>
+						</div>
+					</ButtonGroup>
 				</div>
 			</div>
 		</div>
