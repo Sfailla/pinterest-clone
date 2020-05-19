8673391b580d2fc623f7fcbfa211dfa3b7597c3d
Stevo99 <Sfailla1983@gmail.com>
2020-05-18 20:00:59 -0400

    added isLoading state for api call and to render home component after data loads

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,7 +2,8 @@ import React from 'react';
 import firebase from '../../firebase/firebase';
 
 import Home from '../../pages/home/Home';
-import Boards from '../../pages/boards/Boards';
+// import Home from '../../pages/home/Home1';
+import EasyMasonryComponent from '../../pages/boards/Boards';
 import ViewPin from '../../pages/view/ViewPin';
 import Snackbar from '@material-ui/core/Snackbar';
 import Slide from '@material-ui/core/Slide';
@@ -20,6 +21,7 @@ const SlideTransition = props => {
 
 function Dashboard({ setPage, page, query }) {
 	const [ appData, setAppData ] = React.useState(null);
+	const [ isLoading, setIsLoading ] = React.useState(false);
 	const [ singleViewData, setSingleViewData ] = React.useState(
 		INITIAL_VIEW_STATE
 	);
@@ -28,6 +30,7 @@ function Dashboard({ setPage, page, query }) {
 		Transition: SlideTransition
 	});
 	const [ message, setMessage ] = React.useState('');
+
 	// handles opening the snackbar
 	const handleClick = Transition => {
 		setState({
@@ -35,6 +38,7 @@ function Dashboard({ setPage, page, query }) {
 			Transition
 		});
 	};
+
 	// handles closing the snackbar
 	const handleClose = (event, reason) => {
 		if (reason === 'clickaway') {
@@ -49,16 +53,17 @@ function Dashboard({ setPage, page, query }) {
 	const getInitialData = async () => {
 		const baseUrl = 'https://api.unsplash.com/search/photos?';
 		const client_id = 'qoz2rrh6ChkvTtjYQapHD8P3cXZNi2ZDpG_CD7WBoOU';
+		setIsLoading(true);
 
 		if (appData === null) {
 			const urlParams = `&query=${query}&page=1&per_page=20&client_id=${client_id}`;
 			const result = await fetch(`${baseUrl}${urlParams}`);
 			const data = await result.json();
 
-			return setAppData(data.results);
+			setAppData(data.results);
+			setIsLoading(false);
+			return appData;
 		}
-
-		return;
 	};
 
 	const addPinToBoard = async () => {
@@ -76,12 +81,13 @@ function Dashboard({ setPage, page, query }) {
 					<Home
 						setSingleViewData={setSingleViewData}
 						page={page}
+						isLoading={isLoading}
 						setPage={setPage}
 						data={appData}
 					/>
 				);
 			case 'boards':
-				return <Boards />;
+				return <EasyMasonryComponent data={appData} />;
 			case 'view-pin':
 				return (
 					<ViewPin
@@ -92,18 +98,16 @@ function Dashboard({ setPage, page, query }) {
 					/>
 				);
 			default:
-				return <Home data={appData} />;
+				return console.log('error in switch');
 		}
 	};
 
-	React.useEffect(
-		() => {
-			getInitialData();
-		},
-		[ appData ]
-	);
+	React.useEffect(() => {
+		console.log('use effect firing');
+		getInitialData();
+	}, []);
 
-	console.log(query);
+	console.log('I rendered');
 
 	return (
 		<div className="dashboard">
@@ -114,10 +118,10 @@ function Dashboard({ setPage, page, query }) {
 					horizontal: 'left'
 				}}
 				open={state.open}
-				TransitionComponent={state.Transition}
 				autoHideDuration={3000}
 				onClose={handleClose}
 				message={message}
+				TransitionComponent={state.Transition}
 			/>
 		</div>
 	);
