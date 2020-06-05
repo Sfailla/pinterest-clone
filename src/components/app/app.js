b06c0070c2957b8b3dec1996be29b69469b1b573
Stevo99 <Sfailla1983@gmail.com>
2020-06-05 16:34:53 -0400

    minor changes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,7 +4,6 @@ import PrivateRoute from '../../router/PrivateRoute';
 import PublicRoute from '../../router/PublicRoute';
 
 import Header from '../header/Header.js';
-import Home from '../../pages/home/Home';
 import HomePage from '../../pages/homepage/HomePage.js';
 import Dashboard from '../../pages/dashboard/Dashboard';
 import userAuth from '../user-auth/userAuth';
@@ -13,13 +12,33 @@ function App() {
 	const [ page, setPage ] = React.useState('home');
 	const [ query, setQuery ] = React.useState('guns');
 
+	const searchImages = async event => {
+		event.preventDefault();
+
+		console.log('it runs');
+
+		const baseUrl = 'https://api.unsplash.com/search/photos?';
+		const client_id = 'qoz2rrh6ChkvTtjYQapHD8P3cXZNi2ZDpG_CD7WBoOU';
+		const urlParams = `&query=${query}&page=1&per_page=20&client_id=${client_id}`;
+		const result = await fetch(`${baseUrl}${urlParams}`);
+		const data = await result.json();
+
+		console.log(data);
+	};
+
 	const user = userAuth();
+	console.log(query);
 
 	return (
 		<BrowserRouter>
 			<div className="app-container">
 				{user && (
-					<Header page={page} setQuery={setQuery} setPage={setPage} />
+					<Header
+						page={page}
+						searchImages={searchImages}
+						setQuery={setQuery}
+						setPage={setPage}
+					/>
 				)}
 				<div className="route-container">
 					<Switch>
