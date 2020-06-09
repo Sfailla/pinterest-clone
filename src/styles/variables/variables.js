435cde653f48cfec65085e5da494ba2006d69f86
Stevo99 <Sfailla1983@gmail.com>
2020-06-09 13:20:14 -0400

    made a variables file for all styling vars

---
+++ src/styles/variables/variables.js
@@ -0,0 +1,5 @@
+export const redButton = '#e60023';
+export const redButtonHover = '#ad081b';
+export const greyButton = '#e2e2e2';
+export const greyButtonHover = '#d3d3d3';
+export const facebookButton = '#1877f2';
