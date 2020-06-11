3b2a33d1c77152a262825fa6be42bd089dfbd2f0
Stevo99 <Sfailla1983@gmail.com>
2020-06-11 11:10:55 -0400

    converted sass vars to js

--- src/styles/variables/variables.js
+++ src/styles/variables/variables.js
@@ -3,3 +3,5 @@ export const redButtonHover = '#ad081b';
 export const greyButton = '#e2e2e2';
 export const greyButtonHover = '#d3d3d3';
 export const facebookButton = '#1877f2';
+
+export const mainBorderRadius = '16px';
