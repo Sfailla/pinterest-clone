a8afbe1073c6dcf121652f0ae4be93f345439c8a
Stevo99 <Sfailla1983@gmail.com>
2020-06-17 20:37:54 -0400

    fixed error

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -37,29 +37,29 @@ module.exports = {
 		new webpack.DefinePlugin({
 			'process.env': {
 				FIREBASE_API_KEY: JSON.stringify(
-					Process.env.FIREBASE_API_KEY
+					process.env.FIREBASE_API_KEY
 				),
 				FIREBASE_AUTH_DOMAIN: JSON.stringify(
-					Process.env.FIREBASE_AUTH_DOMAIN
+					process.env.FIREBASE_AUTH_DOMAIN
 				),
 				FIREBASE_PROJECT_ID: JSON.stringify(
-					Process.env.FIREBASE_PROJECT_ID
+					process.env.FIREBASE_PROJECT_ID
 				),
 				FIREBASE_PROJECT_ID: JSON.stringify(
-					Process.env.FIREBASE_PROJECT_ID
+					process.env.FIREBASE_PROJECT_ID
 				),
 				FIREBASE_STORAGE_BUCKET: JSON.stringify(
-					Process.env.FIREBASE_STORAGE_BUCKET
+					process.env.FIREBASE_STORAGE_BUCKET
 				),
 				FIREBASE_SENDER_ID: JSON.stringify(
-					Process.env.FIREBASE_SENDER_ID
+					process.env.FIREBASE_SENDER_ID
 				),
-				FIREBASE_APP_ID: JSON.stringify(Process.env.FIREBASE_APP_ID),
+				FIREBASE_APP_ID: JSON.stringify(process.env.FIREBASE_APP_ID),
 				FIREBASE_MEASUREMENT_ID: JSON.stringify(
-					Process.env.FIREBASE_MEASUREMENT_ID
+					process.env.FIREBASE_MEASUREMENT_ID
 				),
 				UNSPLASH_ACCESS_KEY: JSON.stringify(
-					Process.env.UNSPLASH_ACCESS_KEY
+					process.env.UNSPLASH_ACCESS_KEY
 				)
 			}
 		})
