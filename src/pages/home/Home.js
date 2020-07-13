bd4e78cd27def9372c0e98b1be09ea3aaca69ea7
Stevo99 <Sfailla1983@gmail.com>
2020-07-13 13:31:00 -0400

    refactored home to use masonry custom hook

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,28 +1,18 @@
 import React from 'react';
-import {
-	MasonryScroller,
-	useContainerPosition,
-	usePositioner
-} from 'masonic';
+import { useMasonry, MasonryScroller } from 'masonic';
 import MasonryCard from './MasonryCard';
-import { useWindowSize } from '@react-hook/window-size';
 import { useStyles } from './HomeStyles';
+import useMasonryGrid from '../../hooks/useMasonryGrid';
 
 const Home = ({ data, isLoading, query }) => {
 	const classes = useStyles();
-	const containerRef = React.useRef(null);
-	const [ windowWidth, windowHeight ] = useWindowSize();
 
-	const { offset, width } = useContainerPosition(containerRef, [
-		windowWidth,
-		windowHeight
-	]);
-
-	const positioner = usePositioner({
-		width,
-		columnGutter: 15,
-		columnWidth: 236
-	});
+	const {
+		containerRef,
+		windowHeight,
+		offset,
+		positioner
+	} = useMasonryGrid();
 
 	function ItemArray(id, name, height, src) {
 		this.id = id;
@@ -58,24 +48,20 @@ const Home = ({ data, isLoading, query }) => {
 	return (
 		<div className={classes.root}>
 			<div className={classes.masonic}>
-				{!isLoading ? (
-					<MasonryScroller
-						key={query}
-						positioner={positioner}
-						// The distance in px between the top of the document and the top of the
-						// masonry grid container
-						offset={offset}
-						// The height of the virtualization window
-						height={windowHeight}
-						// Forwards the ref to the masonry container element
-						containerRef={containerRef}
-						items={items}
-						overscanBy={2}
-						render={MasonryCard}
-					/>
-				) : (
-					<div>Loading...</div>
-				)}
+				<MasonryScroller
+					key={query}
+					positioner={positioner}
+					// The distance in px between the top of the document and the top of the
+					// masonry grid container
+					offset={offset}
+					// The height of the virtualization window
+					height={windowHeight}
+					// Forwards the ref to the masonry container element
+					containerRef={containerRef}
+					items={items}
+					overscanBy={2}
+					render={MasonryCard}
+				/>
 			</div>
 		</div>
 	);
