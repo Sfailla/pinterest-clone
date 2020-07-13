d1f338d912a91093d9c0222366f5e3bff2400e0c
Stevo99 <Sfailla1983@gmail.com>
2020-07-13 13:32:01 -0400

    moved images to seperate file so I can dynamically render them

---
+++ src/pages/homepage/images.js
@@ -0,0 +1,23 @@
+import image1 from '../../images/grid-img-1.png';
+import image2 from '../../images/grid-img-2.png';
+import image3 from '../../images/grid-img-3.png';
+import image4 from '../../images/grid-img-4.png';
+import image5 from '../../images/grid-img-5.png';
+import image6 from '../../images/grid-img-6.png';
+import image7 from '../../images/grid-img-7.png';
+import image8 from '../../images/grid-img-8.png';
+import image9 from '../../images/grid-img-9.png';
+import image10 from '../../images/grid-img-10.png';
+
+export default [
+	image1,
+	image2,
+	image3,
+	image4,
+	image5,
+	image6,
+	image7,
+	image8,
+	image9,
+	image10
+];
