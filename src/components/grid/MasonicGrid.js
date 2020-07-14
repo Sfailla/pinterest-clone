ec97c9733d6aae4ec66b2e406671cd8869eb5699
Stevo99 <Sfailla1983@gmail.com>
2020-07-13 22:51:23 -0400

    added custom masonic grid to components which uses masonic hook

---
+++ src/components/grid/MasonicGrid.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import images from '../../pages/homepage/images';
+import useMasonryGrid from '../../hooks/useMasonryGrid';
+import { useStyles } from '../../pages/homepage/HomePageStyles';
+
+function MasonicGrid() {
+	const {
+		positioner,
+		containerRef,
+		scrollTop,
+		windowHeight,
+		useMasonry
+	} = useMasonryGrid();
+
+	const classes = useStyles();
+	const items = React.useMemo(() => getItems(), [ images ]);
+	console.log(items && items);
+
+	return (
+		<div className={classes.root}>
+			{useMasonry({
+				positioner,
+				scrollTop,
+				height: windowHeight,
+				containerRef,
+				items: items,
+				overscanBy: 2,
+				render: ImageCard
+			})}
+		</div>
+	);
+}
+
+export default MasonicGrid;
+
+const ImageCard = ({ data: { src, height, id } }) => {
+	const classes = useStyles({ src, height });
+	return (
+		<div className={classes.masonCard}>
+			<span children={id} />
+		</div>
+	);
+};
+
+const randomChoice = items =>
+	items[Math.floor(Math.random() * items.length)];
+
+const randomHeight = (min = 280, max = 500) =>
+	Math.floor(Math.random() * (max - min)) + min;
+
+const getItems = (cur = 0) => {
+	const items = [];
+	for (let i = 20 * cur; i < cur * 20 + 20; i++)
+		items.push({
+			id: i,
+			src: randomChoice(images),
+			height: randomHeight()
+		});
+	return items;
+};
