6768a1230d38462f9b55ba08d188cef57e0f2fb0
Stevo99 <Sfailla1983@gmail.com>
2020-07-13 22:52:08 -0400

    cleaned up unnecessary files

--- src/components/grid/GridContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useStyles } from './GridStyles';
-
-const GridContainer = ({ children }) => {
-	const classes = useStyles();
-	return <div className={classes.container}>{children}</div>;
-};
-
-export default GridContainer;
