0c47c497397f46956cccd7aad0f2967f5bf76b96
Sfailla <Sfailla1983@gmail.com>
2020-10-26 14:20:37 -0400

    added normalize and changed directory path for app component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'normalize.css';
+import 'normalize.css';
 import './styles/style.scss';
 
-import App from './components/app/app';
+import App from './app/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
