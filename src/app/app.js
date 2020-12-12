580a635fd4d59a8e7e8e09f593f5f733247fa433
Sfailla <Sfailla1983@gmail.com>
2020-12-11 19:28:23 -0500

    moved header and tethered state to dashboard component

--- src/app/app.js
+++ src/app/app.js
@@ -1,70 +1,43 @@
 import React from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import PrivateRoute from '../router/PrivateRoute';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { PrivateRoute, PublicRoute } from '../router';
+import * as ROUTES from '../constants/Routes';
 
-import Header from '../components/header/Header.js';
 import HomePage from '../pages/homepage/HomePage.js';
 import Dashboard from '../pages/dashboard/Dashboard';
 import useAuth from '../hooks/useAuth';
-import useFetch from '../hooks/useFetch';
+
+import Loader from '../components/loader/Loader';
 
 function App() {
-	const [ query, setQuery ] = React.useState('cars');
-	const [ page, setPage ] = React.useState('home');
-
-	const url = 'https://api.unsplash.com/search/photos?';
-	const params = {
-		query,
-		page: 1,
-		per_page: 20,
-		client_id: process.env.UNSPLASH_ACCESS_KEY
-	};
-
-	const { data, isDataLoading, updateParams } = useFetch(url, params);
 	const { user, isAuthPending } = useAuth();
-
-	const updateFetchResults = event => {
-		event.preventDefault();
-		updateParams(params);
-	};
 
 	return (
 		<Router>
-			<div className="app-container">
-				{user && (
-					<Header
-						page={page}
-						updateFetchResults={updateFetchResults}
-						setQuery={setQuery}
-						setPage={setPage}
-					/>
-				)}
+			{isAuthPending ? (
+				<Loader />
+			) : (
+				<div className="app-container">
+					<div className="route-container">
+						<Switch>
+							<PublicRoute
+								exact
+								path={ROUTES.HOME}
+								component={HomePage}
+								restricted={!!user}
+								redirect={ROUTES.DASHBOARD}
+							/>
 
-				<div className="route-container">
-					<Switch>
-						<Route
-							exact
-							path="/"
-							user={user}
-							render={() =>
-								isAuthPending || user ? <Redirect to="/dashboard" /> : <HomePage />}
-						/>
-
-						<PrivateRoute
-							exact
-							path="/dashboard"
-							page={page}
-							data={data}
-							query={query}
-							user={user}
-							isLoading={isAuthPending}
-							isDataLoading={isDataLoading}
-							setPage={setPage}
-							component={Dashboard}
-						/>
-					</Switch>
+							<PrivateRoute
+								path={ROUTES.DASHBOARD}
+								user={user}
+								component={Dashboard}
+								redirect={ROUTES.HOME}
+							/>
+						</Switch>
+					</div>
 				</div>
-			</div>
+			)}
 		</Router>
 	);
 }
