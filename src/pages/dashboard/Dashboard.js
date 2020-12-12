73e295e0e258f9c2327410a50be39fe5d3721a63
Sfailla <Sfailla1983@gmail.com>
2020-12-12 18:31:05 -0500

    changed home component to search

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import firebase from '../../firebase/firebase';
 import { useStyles } from './DashboardStyles';
 import useFetch from '../../hooks/useFetch';
 
-import Home from '../../pages/home/Home';
+import Search from '../search/Search';
 import Snackbar from '@material-ui/core/Snackbar';
 import Slide from '@material-ui/core/Slide';
 import Header from '../../components/header/Header';
@@ -14,7 +14,7 @@ const SlideTransition = props => {
 	return <Slide {...props} direction="up" />;
 };
 
-function Dashboard() {
+export default function Dashboard() {
 	const classes = useStyles();
 
 	const [ state, setState ] = React.useState({
@@ -60,7 +60,7 @@ function Dashboard() {
 	const [ singleViewData, setSingleViewData ] = React.useState(null);
 	const [ appData, setAppData ] = React.useState([]);
 	const [ dataError, setDataError ] = React.useState(null);
-	const [ page, setPage ] = React.useState('home');
+	const [ page, setPage ] = React.useState('search');
 	const [ query, setQuery ] = React.useState('cars');
 	const url = 'https://api.unsplash.com/search/photos?';
 	const params = {
@@ -104,9 +104,9 @@ function Dashboard() {
 					setPage={setPage}
 				/>
 				<div className={classes.root}>
-					<h1 className={classes.title}>images for "{query}"</h1>
+					<h1 className={classes.title}>search for "{query}"</h1>
 					{appData.length > 0 && (
-						<Home
+						<Search
 							setSingleViewData={setSingleViewData}
 							page={page}
 							isDataLoading={isDataLoading}
@@ -114,6 +114,7 @@ function Dashboard() {
 							items={items}
 						/>
 					)}
+
 					<Snackbar
 						anchorOrigin={{
 							vertical: 'bottom',
@@ -130,4 +131,3 @@ function Dashboard() {
 		</React.Fragment>
 	);
 }
-export default Dashboard;
---
+++ src/pages/search/SearchStyles.js
@@ -0,0 +1,47 @@
+import { makeStyles } from '@material-ui/core';
+import { mainBorderRadius } from '../../styles';
+
+export const useStyles = makeStyles(theme => ({
+	root: {
+		width: '100%',
+		height: '100vh',
+		minHeight: '100vh'
+	},
+	title: {
+		padding: theme.spacing(2, 0)
+	},
+	masonic: {
+		padding: theme.spacing(1, 3)
+	},
+	container: {
+		width: '100%',
+		minHeight: '100vh'
+	},
+	masonCard: {
+		width: '100%',
+		height: props => `${props.height}px`,
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		borderRadius: mainBorderRadius,
+		background: props => `url(${props.src}) no-repeat center / cover`,
+		transition: theme.transitions.create('transform'),
+		'& span': {
+			width: '100%',
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			background: 'rgb(161, 41, 41)',
+			color: theme.palette.common.white,
+			padding: theme.spacing(1),
+			letterSpacing: '3px',
+			borderBottomLeftRadius: '10px',
+			borderBottomRightRadius: '10px'
+		}
+	},
+	button: {
+		width: '100%',
+		height: ({ height }) => height + 'px'
+	}
+}));
