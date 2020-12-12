653319a45f2eb819493e90e30f8eecc9c20cef7b
Sfailla <Sfailla1983@gmail.com>
2020-12-12 13:47:34 -0500

    added Card Context to pass onClick handler to MasonCard

--- src/pages/home/MasonryCard.js
+++ src/pages/home/MasonryCard.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { useStyles } from './HomeStyles';
+import CardContext from '../dashboard/cardContext';
 
-const MasonryCard = ({ data: { name, src, height } }) => {
+const MasonryCard = ({ data: { id, name, src, height } }) => {
 	const classes = useStyles({ src, height });
+	const { setSingleViewData } = React.useContext(CardContext);
+	const handleClick = React.useCallback(() => {
+		setSingleViewData({
+			id,
+			name,
+			src
+		});
+	}, []);
+
 	return (
-		<div className={classes.masonCard}>
-			<span children={name.toUpperCase()} />
-		</div>
+		<button onClick={handleClick} className={classes.button}>
+			<div className={classes.masonCard}>
+				<span children={name.toUpperCase()} />
+			</div>
+		</button>
 	);
 };
 
