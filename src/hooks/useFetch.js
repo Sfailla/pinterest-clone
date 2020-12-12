68df768b9b2d784a96cbedc35ba0a87095b178c4
Sfailla <Sfailla1983@gmail.com>
2020-12-12 13:51:13 -0500

    added error handling to useFetch hook. removed unnecessary code

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,56 +3,53 @@ import React from 'react';
 function useFetch(initialUrl, initialParams = {}, skip = false) {
 	const [ url, updateUrl ] = React.useState(initialUrl);
 	const [ params, updateParams ] = React.useState(initialParams);
+	const [ error, setError ] = React.useState({
+		fetch: '',
+		results: ''
+	});
 	const [ hasError, setHasError ] = React.useState(false);
-	const [ errorMessage, setErrorMessage ] = React.useState('');
-	const [ isDataLoading, setIsDataLoading ] = React.useState(false);
+	const [ isDataLoading, setIsDataLoading ] = React.useState(true);
 	const [ data, setData ] = React.useState(null);
-	const [ refetchIndex, setRefetchIndex ] = React.useState(0);
 
 	const queryString = Object.keys(params)
-		.map(
-			key =>
-				encodeURIComponent(key) +
-				'=' +
-				encodeURIComponent(params[key])
-		)
+		.map(key => encodeURIComponent(key) + '=' + encodeURIComponent(params[key]))
 		.join('&');
-
-	const refetch = () =>
-		setRefetchIndex(prevRefetchIndex => prevRefetchIndex + 1);
 
 	React.useEffect(
 		() => {
 			const fetchData = async () => {
-				if (skip) return setIsDataLoading(true);
 				try {
+					setIsDataLoading(true);
 					const response = await fetch(`${url}${queryString}`);
 					const result = await response.json();
 					if (response.ok) {
-						setData(result.results);
+						if (result.total > 19) {
+							setData(result.results);
+						} else {
+							setError({ results: 'sorry no results found' });
+						}
 					} else {
 						setHasError(true);
-						setErrorMessage(result);
+						setError({ fetch: result });
 					}
 				} catch (error) {
 					setHasError(true);
-					setErrorMessage(error.message);
+					setError({ fetch: error.message });
 				} finally {
 					setIsDataLoading(false);
 				}
 			};
 			fetchData();
 		},
-		[ url, params, refetchIndex ]
+		[ url, params ]
 	);
 	return {
 		data,
 		isDataLoading,
 		hasError,
-		errorMessage,
+		error,
 		updateUrl,
-		updateParams,
-		refetch
+		updateParams
 	};
 }
 
