ea98633ac216553bbf7c5dc9ed788319020ebea0
Sfailla <Sfailla1983@gmail.com>
2020-12-11 19:36:07 -0500

    added more imaes for homepage

--- src/pages/homepage/images.js
+++ src/pages/homepage/images.js
@@ -8,6 +8,16 @@ import image7 from '../../images/grid-img-7.png';
 import image8 from '../../images/grid-img-8.png';
 import image9 from '../../images/grid-img-9.png';
 import image10 from '../../images/grid-img-10.png';
+import image11 from '../../images/grid-img-11.png';
+import image12 from '../../images/grid-img-12.png';
+import image13 from '../../images/grid-img-13.png';
+import image14 from '../../images/grid-img-14.png';
+import image15 from '../../images/grid-img-15.png';
+import image16 from '../../images/grid-img-16.png';
+import image17 from '../../images/grid-img-17.png';
+import image18 from '../../images/grid-img-18.png';
+import image19 from '../../images/grid-img-19.png';
+import image20 from '../../images/grid-img-20.png';
 
 export default [
 	image1,
@@ -19,5 +29,15 @@ export default [
 	image7,
 	image8,
 	image9,
-	image10
+	image10,
+	image11,
+	image12,
+	image13,
+	image14,
+	image15,
+	image16,
+	image17,
+	image18,
+	image19,
+	image20
 ];
