6c21a97d187ea2a9e515f4ed8e2c646dc811afeb
Sfailla <Sfailla1983@gmail.com>
2020-12-15 14:42:43 -0500

    cleaned up dashboard. removed unnecessary functions and state.  added useForm hook

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import firebase from '../../firebase/firebase';
 import { useStyles } from './DashboardStyles';
 import useFetch from '../../hooks/useFetch';
+import useForm from '../../hooks/useForm';
 
 import Search from '../search/Search';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -9,6 +10,7 @@ import Slide from '@material-ui/core/Slide';
 import Header from '../../components/header/Header';
 import { createGridItems } from './createGridItems';
 import CardContext from './cardContext';
+import validateSearch from '../../utils/form-validation/validateSearch';
 
 const SlideTransition = props => {
 	return <Slide {...props} direction="up" />;
@@ -57,38 +59,42 @@ export default function Dashboard() {
 	// 	}
 	// };
 
-	const [ singleViewData, setSingleViewData ] = React.useState(null);
-	const [ appData, setAppData ] = React.useState([]);
-	const [ dataError, setDataError ] = React.useState(null);
-	const [ page, setPage ] = React.useState('search');
-	const [ query, setQuery ] = React.useState('cars');
-	const url = 'https://api.unsplash.com/search/photos?';
+	const { handleOnChange, handleOnSubmit, values, errors } = useForm(
+		{ search: '' },
+		validateSearch,
+		authorize
+	);
+
 	const params = {
-		query,
+		query: values.search || 'cars',
 		page: 1,
 		per_page: 30,
 		client_id: process.env.UNSPLASH_ACCESS_KEY
 	};
 
-	const { data, isDataLoading, updateParams, error } = useFetch(url, params);
+	const { data, isDataLoading, updateParams, error } = useFetch(
+		'https://api.unsplash.com/search/photos?',
+		params
+	);
 
-	console.log(error);
+	const [ singleViewData, setSingleViewData ] = React.useState(null);
+	const [ appData, setAppData ] = React.useState([]);
+	const [ page, setPage ] = React.useState('search');
 
 	React.useEffect(
 		() => {
-			if (!isDataLoading) {
+			if (!isDataLoading && !error) {
 				setAppData(data);
-			} else {
-				setDataError('no search results found');
+			} else if (error) {
+				// setSearchError(error);
 			}
 		},
 		[ appData, isDataLoading ]
 	);
 
-	const updateFetchResults = event => {
-		event.preventDefault();
+	function authorize() {
 		updateParams(params);
-	};
+	}
 
 	const items = createGridItems(appData);
 
@@ -99,12 +105,12 @@ export default function Dashboard() {
 			<CardContext.Provider value={{ setSingleViewData }}>
 				<Header
 					page={page}
-					updateFetchResults={updateFetchResults}
-					setQuery={setQuery}
 					setPage={setPage}
+					handleOnChange={handleOnChange}
+					handleOnSubmit={handleOnSubmit}
 				/>
 				<div className={classes.root}>
-					<h1 className={classes.title}>search for "{query}"</h1>
+					<h1 className={classes.title}>search for "{'cars'}"</h1>
 					{appData.length > 0 && (
 						<Search
 							setSingleViewData={setSingleViewData}
