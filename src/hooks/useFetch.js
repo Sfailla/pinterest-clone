a11599039a8ed431d05bfdb946cdc4e129825bbd
Sfailla <Sfailla1983@gmail.com>
2020-12-17 09:42:00 -0500

    formatted page on save after prettier activation

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,54 +1,51 @@
-import React from 'react';
+import React from "react";
 
 function useFetch(initialUrl, initialParams = {}) {
-	const [ url, updateUrl ] = React.useState(initialUrl);
-	const [ params, updateParams ] = React.useState(initialParams);
-	const [ fetchErrors, setFetchErrors ] = React.useState('');
-	const [ hasError, setHasError ] = React.useState(false);
-	const [ isDataLoading, setIsDataLoading ] = React.useState(true);
-	const [ data, setData ] = React.useState([]);
+  const [url, updateUrl] = React.useState(initialUrl);
+  const [params, updateParams] = React.useState(initialParams);
+  const [fetchErrors, setFetchErrors] = React.useState("");
+  const [hasError, setHasError] = React.useState(false);
+  const [isDataLoading, setIsDataLoading] = React.useState(true);
+  const [data, setData] = React.useState([]);
 
-	const queryString = Object.keys(params)
-		.map(key => encodeURIComponent(key) + '=' + encodeURIComponent(params[key]))
-		.join('&');
+  const queryString = Object.keys(params)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(params[key]))
+    .join("&");
 
-	React.useEffect(
-		() => {
-			const fetchData = async () => {
-				try {
-					setIsDataLoading(true);
-					const response = await fetch(`${url}${queryString}`);
-					const result = await response.json();
-					if (response.ok) {
-						if (result.total > 19) {
-							setData(result.results);
-						} else {
-							setFetchErrors('sorry no results found');
-						}
-					} else {
-						setHasError(true);
-						setFetchErrors(result);
-					}
-				} catch (error) {
-					setHasError(true);
-					setFetchErrors(error.message);
-				} finally {
-					setIsDataLoading(false);
-				}
-			};
-			fetchData();
-		},
-		[ url, params ]
-	);
-	return {
-		data,
-		isDataLoading,
-		hasError,
-		setFetchErrors,
-		fetchErrors,
-		updateUrl,
-		updateParams
-	};
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsDataLoading(true);
+        const response = await fetch(`${url}${queryString}`);
+        const result = await response.json();
+        if (response.ok) {
+          if (result.total > 19) {
+            setData(result);
+          } else {
+            setFetchErrors("sorry no results found");
+          }
+        } else {
+          setHasError(true);
+          setFetchErrors(result);
+        }
+      } catch (error) {
+        setHasError(true);
+        setFetchErrors(error.message);
+      } finally {
+        setIsDataLoading(false);
+      }
+    };
+    fetchData();
+  }, [url, params]);
+  return {
+    data,
+    isDataLoading,
+    hasError,
+    setFetchErrors,
+    fetchErrors,
+    updateUrl,
+    updateParams,
+  };
 }
 
 export default useFetch;
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,49 +1,46 @@
-import React from 'react';
+import React from "react";
 
 export default function useForm(initialState, validate, authenticate) {
-	const [ values, setValues ] = React.useState(initialState);
-	const [ formErrors, setFormErrors ] = React.useState({});
-	const [ isSubmitting, setSubmitting ] = React.useState(false);
+  const [values, setValues] = React.useState(initialState);
+  const [formErrors, setFormErrors] = React.useState({});
+  const [isSubmitting, setSubmitting] = React.useState(false);
 
-	React.useEffect(
-		() => {
-			if (isSubmitting) {
-				const noErrors = Object.keys(formErrors).length === 0;
-				if (noErrors) {
-					setSubmitting(false);
-					setValues(initialState);
-					authenticate();
-				} else {
-					console.log('authentication error');
-					setSubmitting(false);
-				}
-			}
-		},
-		[ formErrors, isSubmitting, authenticate, setValues ]
-	);
+  React.useEffect(() => {
+    if (isSubmitting) {
+      const noErrors = Object.keys(formErrors).length === 0;
+      if (noErrors) {
+        setSubmitting(false);
+        setValues(initialState);
+        authenticate();
+      } else {
+        console.log("authentication error");
+        setSubmitting(false);
+      }
+    }
+  }, [formErrors, isSubmitting, authenticate, setValues]);
 
-	const handleOnChange = event => {
-		event.persist();
+  const handleOnChange = event => {
+    event.persist();
 
-		setValues(prevState => ({
-			...prevState,
-			[event.target.name]: event.target.value
-		}));
-	};
+    setValues(prevState => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
-	const handleOnSubmit = event => {
-		event.preventDefault();
-		let errors = validate(values);
-		setFormErrors(errors);
-		setSubmitting(true);
-	};
+  const handleOnSubmit = event => {
+    event.preventDefault();
+    let errors = validate(values);
+    setFormErrors(errors);
+    setSubmitting(true);
+  };
 
-	return {
-		handleOnChange,
-		handleOnSubmit,
-		isSubmitting,
-		values,
-		setFormErrors,
-		formErrors
-	};
+  return {
+    handleOnChange,
+    handleOnSubmit,
+    isSubmitting,
+    values,
+    setFormErrors,
+    formErrors,
+  };
 }
